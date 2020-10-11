a3e663c180cd722bbf249309e9b0f5e20bcd7689
Ivan Savov <ivan.savov@gmail.com>
2020-10-11 00:12:25 -0400

    Docker installed on remote host

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docker-compose-traefik-acme
+
 A sample project that uses docker-compose and traefik for HTTP ingress, including Let's Encrypt certs.
+
+
+Install fabric
+--------------
+In order to automate the steps docker host provisioning, we'll use Fabric,
+which means we need to setup a Python environment for this project.
+
+    virtualenv -p python3 venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+
+Configure remote host
+---------------------
+Edit `fabfile.py` and edit the values of `env.hosts` and `env.user` setting them
+to the info of the new hosts (IP address and admin user with sudo capabilities).
+
+
+Install docker on remote host
+-----------------------------
+
+    fab install_docker
+
+
---
+++ fabfile.py
@@ -0,0 +1,57 @@
+#/usr/bin/env python
+from datetime import datetime
+import os
+import uuid
+
+from fabric.api import env, task, put, sudo, local, cd, lcd, run, prompt
+from fabric.colors import red, green, blue, yellow
+from fabric.api import shell_env
+from fabric.context_managers import cd, prefix, settings, hide
+
+
+
+# HOST INFO (manually provisioned in AWS console + open ports 21, 80, 433, 8080)
+################################################################################
+env.hosts = ['3.86.128.130']
+env.user = 'admin'
+
+
+
+# PROVISION DOCKER ON REMOTE HOST
+################################################################################
+
+@task
+def install_docker():
+    """
+    Install docker on remote host following the instructions from the docs:
+    https://docs.docker.com/engine/install/debian/#install-using-the-repository
+    """
+    with settings(warn_only=True), hide('stdout', 'stderr', 'warnings'):
+        sudo('apt-get -qy remove docker docker-engine docker.io containerd runc')
+    with hide('stdout'):
+        sudo('apt-get update -qq')
+        sudo('apt-get -qy install apt-transport-https ca-certificates curl gnupg-agent software-properties-common')
+    sudo('curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -')
+    sudo('add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"')
+    with hide('stdout'):
+        sudo('apt-get update -qq')
+        sudo('apt-get -qy install docker-ce docker-ce-cli containerd.io')
+    print(green('Docker installed on ' + env.host))
+
+
+@task
+def uninstall_docker(deep=False):
+    """
+    Uninstall docker using instructions from the official Docker docs:
+    https://docs.docker.com/engine/install/debian/#uninstall-docker-engine
+    """
+    deep = (deep and deep.lower() == 'true')  # defaults to False
+    with hide('stdout'):
+        sudo('apt-get -qy purge docker-ce docker-ce-cli containerd.io')
+        if deep:
+            sudo('rm -rf /var/lib/docker')
+            sudo('rm -rf /var/lib/containerd')
+    print(green('Docker uninstalled on ' + env.host))
+
+
+
